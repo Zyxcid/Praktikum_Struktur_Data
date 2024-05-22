430e0e559adda7cf9ccfd22c37b357b083273c27
Sha <121550192+Zyxcid@users.noreply.github.com>
2024-05-22 22:18:13 +0700

    Add files via upload

---
+++ Job Sheet 06/Latihan/nomor1.c
@@ -0,0 +1,78 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int MAXSIZE = 8;
+int stack[8];
+int top = -1;
+
+int isempty()
+{
+    if (top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int isfull()
+{
+    if (top == MAXSIZE - 1)
+        return 1;
+    else
+        return 0;
+}
+
+int peek()
+{
+    return stack[top];
+}
+
+int pop()
+{
+    int data;
+    if (!isempty())
+    {
+        data = stack[top];
+        top = top - 1;
+        return data;
+    }
+    else
+    {
+        printf("[ Tidak dapat mengambil data, Stack kosong ]\n");
+    }
+}
+
+int push(int data)
+{
+    if (!isfull())
+    {
+        top = top + 1;
+        stack[top] = data;
+    }
+    else
+    {
+        printf("[ Tidak dapat menambahkan data. Stack penuh ]\n");
+    }
+}
+
+int main()
+{
+    push(3);
+    push(5);
+    push(9);
+    push(1);
+    push(12);
+    push(15);
+    printf("Elemen teratas pada Stack adalah: %d\n", peek());
+    printf("Semua elemennya adalah: \n");
+
+    while (!isempty())
+    {
+        int data = pop();
+        printf("%d\n", data);
+    }
+
+    printf("Stack full: %s\n", isfull() ? "true" : "false");
+    printf("Stack empty: %s\n", isempty() ? "true" : "false");
+    return 0;
+}
